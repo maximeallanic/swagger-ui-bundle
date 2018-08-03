d78e694dac9e3a7f7daa906d77995ac11177dcc9
Maxime Allanic <maxime@allanic.me>
2018-08-03 15:00:16 +0200

    {UPDATE] to latest ui

--- Resources/public/plugin.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2018 Allanic.me ISC License License
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * Created by mallanic <maxime@allanic.me> at 18/07/2018
- */
